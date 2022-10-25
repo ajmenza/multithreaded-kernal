1ffe59d6f74f3112d665b5a498bb8283fa99bc15
tony <ajm043@bucknell.edu>
2022-10-25 15:15:38 -0400

    BIOS parameter block

--- boot.asm
+++ boot.asm
@@ -1,7 +1,15 @@
 ORG 0
 BITS 16
+_start:
+    jmp short start
+    nop
+
+ times 33 db 0 ; create 33 bytes for BIOS Parameter Block
 
 start:
+    jmp 0x7c0:step2
+
+step2:
     cli ; Clear interupts (disable interupts, critical operations)
     mov ax, 0x7c0
     mov ds, ax
@@ -10,6 +18,7 @@ start:
     mov ss, ax
     mov sp, 0x7c00
     sti ; Enables interupts
+
     mov si, message
     call print
     jmp $
